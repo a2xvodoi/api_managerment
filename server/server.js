301bbe07fb2da14a7f8c84dd001d35cffd3c9d7c
viet.nd <viet.nd@beetechsoft.vn>
2022-10-27 14:58:55 +0700

    netlify config

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import serverless from "serverless-http";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+import "dotenv/config";
+import router from "./routes";
+import { connect } from "./config/db";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+    express.urlencoded({
+        limit: "50mb",
+        extended: true,
+    })
+);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use(cors());
+// Connect db
+connect();
+// Router
+router(app);
+
+export default serverless(app);
